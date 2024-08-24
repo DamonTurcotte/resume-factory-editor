82736a5196a9f694624cb067c011a465c94d37b8
Damon Turcotte <damon.j.turcotte@gmail.com>
2024-08-24 03:11:55 -0600

    build: initialize and scaffold text editor

---
+++ packages/TextEditor/src/TextEditor.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View } from "react-native";
+
+const editorHtml = require("resume-factory-editor-web");
+
+export const TextEditor = () => {
+  console.log(editorHtml);
+  return <View />;
+};
+
+export default TextEditor;
