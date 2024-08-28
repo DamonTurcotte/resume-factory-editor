8fd95a5633b684ac79f8eb744fa685da486e6c62
Damon Turcotte <damon.j.turcotte@gmail.com>
2024-08-28 06:15:42 -0600

    chore: update main.ts to integrate Quill editor and add toolbar module

--- packages/webEditor/src/main.ts
+++ packages/webEditor/src/main.ts
@@ -1,16 +1,12 @@
-import './style.css'
-import { setupCounter } from './counter'
+import Quill from 'quill'
+import 'quill/dist/quill.snow.css'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+export const quill = new Quill('#editor', {
+  debug: 'info',
+  theme: 'snow',
+  modules: {
+    toolbar: true,
+  },
+  placeholder: 'Compose an epic...',
+})
